f4228b5457dff3e09a6b0e6815e0e4c816f053c8
Hassan <sychox2006@gmail.com>
2025-08-10 11:14:53 +0500

    Create auth.js

---
+++ api/auth.js
@@ -0,0 +1,20 @@
+const { google } = require('googleapis');
+
+const oauth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URI
+);
+
+module.exports = (req, res) => {
+  const url = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: [
+      'https://www.googleapis.com/auth/drive.file',
+      'https://www.googleapis.com/auth/userinfo.profile',
+      'https://www.googleapis.com/auth/userinfo.email'
+    ],
+  });
+  res.writeHead(302, { Location: url });
+  res.end();
+};
